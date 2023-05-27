54468c43fb3bc827e1584aa9965bf12e48e6abc7
taryb <tbounavong@gmail.com>
2023-05-27 14:50:27 -0700

    changed title

--- fishloggg/src/App.vue
+++ fishloggg/src/App.vue
@@ -7,7 +7,7 @@
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Fishing Log</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
