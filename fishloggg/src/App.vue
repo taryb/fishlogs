f521ad59d728b599f5dec3fae61cb937ea6a75ca
taryb <tbounavong@gmail.com>
2023-05-27 17:40:13 -0700

    end here

--- fishloggg/src/App.vue
+++ fishloggg/src/App.vue
@@ -7,7 +7,7 @@
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Fishing Log</v-toolbar-title>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -16,7 +16,7 @@
   </v-app>
 </template>
 
-<script lang="ts">
+<script>
   export default {
     data: () => ({ drawer: null }),
   }
