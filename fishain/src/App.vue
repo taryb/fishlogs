52078db0e00f14276ab97a1f82fe8eab700105a5
taryb <tbounavong@gmail.com>
2023-05-27 19:55:06 -0700

    added wireframe and title

--- fishain/src/App.vue
+++ fishain/src/App.vue
@@ -1,7 +1,27 @@
-<template>
-  <router-view />
-</template>
 
 <script setup>
   //
+</script>
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Fishing Log</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
